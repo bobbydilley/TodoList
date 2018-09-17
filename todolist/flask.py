2b3b91bbccdb439b0d2395a910c960e3818434dd
Bobby Dilley <bobbydilley@live.com>
2018-09-17 09:58:59 +0100

    Fixed some of the inital code

--- todolist/flask.py
+++ todolist/flask.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify(status = "Failed")
+    return jsonify({'status' : 'online'})
 
 if __name__ == '__main__':
     app.run(debug=True)
