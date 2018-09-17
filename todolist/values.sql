b2b75b95648fa1542e3952748e0e5d814876a1e5
Bobby Dilley <bobbydilley@live.com>
2018-09-17 09:57:14 +0100

    Moved files around

---
+++ todolist/values.sql
@@ -0,0 +1,6 @@
+INSERT INTO Tasks (Description) VALUES ('Remember to take out the bins');
+INSERT INTO Tasks (Description) VALUES ('Renew passport');
+INSERT INTO Tasks (Description) VALUES ('Call driving instructor');
+
+INSERT INTO Tags (TagName, TaskID) VALUES ('passport', 2);
+
