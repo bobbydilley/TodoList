b3ae8ecd9ca4831106573d51d38a0cd1ebd3ec15
Bobby Dilley <bobbydilley@live.com>
2018-10-18 15:17:55 +0100

    Update todolist application

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def stat(path):
     return send_from_directory('static', path)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=80)
