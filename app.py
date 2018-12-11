ebdc995848c74d4cecaacd62341d21b9f5b7f688
Bobby Dilley <bobbydilley@live.com>
2018-12-11 16:17:13 +0000

    Add wsgi files for running properly w

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request, send_from_directory
 from database import Database
 
 app = Flask(__name__)
-
+databasefp = '/home/bobbydilley/todo.db'
 @app.route('/')
 def index():
     return send_from_directory('static', 'login.html')
@@ -21,44 +21,44 @@ def logout():
 
 @app.route('/newTask', methods = ['POST'])
 def new_task():
-    database = Database('todo.db')
+    database = Database(databasefp)
     database.new_task(request.form['description'], request.headers.get('X-API-Key'))
     return jsonify({'status' : 'created'})
 
 @app.route('/removeTask/<task_id>')
 def remove_task(task_id):
-    database = Database('todo.db')
+    database = Database(databasefp)
     database.remove_task(task_id, request.headers.get('X-API-Key'))
     return jsonify({'status' : 'removed'})
 
 @app.route('/login', methods = ['POST'])
 def login():
-    database = Database('todo.db')
+    database = Database(databasefp)
     return database.login(request.form['username'], request.form['password'])
 
 @app.route('/<api_key>.ics')
 def generate_ical(api_key):
-    database = Database('todo.db')
+    database = Database(databasefp)
     return database.generate_ical(api_key)
 
 @app.route('/tags')
 def get_tags():
-    database = Database('todo.db')
+    database = Database(databasefp)
     return jsonify(database.get_tags(request.headers.get('X-API-Key')))
 
 @app.route('/tasks')
 def get_tasks():
-    database = Database('todo.db')
+    database = Database(databasefp)
     return jsonify(database.get_tasks(request.headers.get('X-API-Key')))
 
 @app.route('/tasks/<tag>')
 def get_tasks_tag(tag):
-    database = Database('todo.db')
+    database = Database(databasefp)
     return jsonify(database.get_tasks_tag(tag, request.headers.get('X-API-Key')))
 
 @app.route('/snoozeTask/<task_id>')
 def snooze_task(task_id):
-    database = Database('todo.db')
+    database = Database(databasefp)
     database.snooze_task(task_id, request.headers.get('X-API-Key'))
     return jsonify({'status' : 'snoozed'})
 
---
+++ run.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, '/var/www/todolist/todolist')
+from app import app as application
