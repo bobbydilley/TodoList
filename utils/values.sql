03a02829aa8d3e691f6ca8f57d0e89442154f5e7
Bobby Dilley <bobbydilley@users.noreply.github.com>
2025-07-10 17:20:25 +0100

    Update values.sql

--- utils/values.sql
+++ utils/values.sql
@@ -1,5 +1,5 @@
-INSERT INTO Users (Username, Password) VALUES ('bobbydilley', '$pbkdf2-sha256$29000$IKQ0ZowRIkQohbC2FoIwhg$9gMf8YkfzAU7GykhUoBesxMPvZPA2zDtlBQ5hhShK7g');
-INSERT INTO Users (Username, Password) VALUES ('janedilley', '$pbkdf2-sha256$29000$IKQ0ZowRIkQohbC2FoIwhg$9gMf8YkfzAU7GykhUoBesxMPvZPA2zDtlBQ5hhShK7g');
+INSERT INTO Users (Username, Password) VALUES ('bobbydilley', '-');
+INSERT INTO Users (Username, Password) VALUES ('janedilley', '-');
 
 INSERT INTO Tasks (Description, Username) VALUES ('Remember to take out the bins', 'bobbydilley');
 INSERT INTO Tasks (Description, Username) VALUES ('Renew passport', 'bobbydilley');
